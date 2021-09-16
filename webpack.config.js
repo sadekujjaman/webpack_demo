65bec61ecd73d612b345b5f7c0496be7e79a3b61
Sadekujjaman Saju <sadekujjamansaju474@gmail.com>
2021-09-17 04:55:04 +0600

    css style using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
@@ -13,4 +13,13 @@ module.exports = {
     compress: true,
     port: 3000,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
 };
